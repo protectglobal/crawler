86c847e41bf499beaf38e56a2d26ba27054989a5
fede-rodes <rodesf@gmail.com>
2018-01-22 21:05:03 -0300

    catch 400 error on crawler

--- imports/api/crawler.js
+++ imports/api/crawler.js
@@ -60,14 +60,14 @@ function visitPage(url, callback, cb) {
 
   request(url, (error, response, body) => {
     // Check status code (200 is HTTP OK)
-    if (!error && response && response.statusCode && response.statusCode === 404) {
+    if (!error && response.statusCode !== 200) {
       console.log('\nNOT FOUND', response && response.statusCode);
       // Skip this page.
       callback(cb);
       return;
     }
 
-    if (!!error || !response || !response.statusCode || !body || response.statusCode !== 200) {
+    if (error) {
       console.log('\nerror', error);
       // console.log('\nresponse', response);
       console.log('\nresponse.statusCode', response && response.statusCode);
